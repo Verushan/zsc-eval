6e01f3bca8c214a02000fb3dfbce6d64fad1c25c
Verushan <verushannaidoo@gmail.com>
2026-05-25 21:00:15 +0200

    Update self play train script with aligned parameters

--- zsceval/scripts/overcooked/shell/train_sp.sh
+++ zsceval/scripts/overcooked/shell/train_sp.sh
@@ -10,6 +10,7 @@ fi
 
 entropy_coefs="0.2 0.05 0.01"
 entropy_coef_horizons="0 5e6 1e7"
+
 if [[ "${layout}" == "small_corridor" ]]; then
     entropy_coefs="0.2 0.05 0.01"
     entropy_coef_horizons="0 8e6 1e7"
@@ -17,6 +18,10 @@ fi
 
 reward_shaping_horizon="1e8"
 num_env_steps="1e7"
+episode_length=400
+ppo_epoch=15
+dummy_batch_size=2
+num_mini_batch=4
 
 num_agents=2
 algo="mappo"
@@ -30,9 +35,10 @@ for seed in $(seq ${seed_begin} ${seed_max});
 do
     echo "seed is ${seed}:"
     python train/train_sp.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --layout_name ${layout} --num_agents ${num_agents} \
-    --seed ${seed} --n_training_threads $TRAINING_THREADS --n_rollout_threads $ROLLOUT_THREADS --dummy_batch_size 2 --num_mini_batch 1 --episode_length 25 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
+    --seed ${seed} --n_training_threads $TRAINING_THREADS --n_rollout_threads $ROLLOUT_THREADS --dummy_batch_size ${dummy_batch_size} --num_mini_batch ${num_mini_batch} --episode_length ${episode_length} --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
     --overcooked_version ${version} \
-    --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
+    --data-parallel \
+    --ppo_epoch ${ppo_epoch} --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
     --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_recurrent_policy \
     --use_proper_time_limits \
     --save_interval 25 --log_interval 10 --use_eval --eval_interval 20 --n_eval_rollout_threads 10 \
